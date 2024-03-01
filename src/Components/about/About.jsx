c7a7a4a4dca6db4d61a7f7505a4fb8c9b6532ffb
Mahmoud khairy <74417372+mahmoudkhairy402@users.noreply.github.com>
2024-03-01 06:39:23 +0200

    download CV commit

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import "../about/about.css";
-import {
-  Bounce,
-  Fade,
-  Hinge,
-  JackInTheBox,
-  Roll,
-  Rotate,
-  Zoom,
-} from "react-awesome-reveal";
+import { saveAs } from "file-saver";
+import { Fade, Roll } from "react-awesome-reveal";
+import localPdfFile from "../../assets/resume.pdf"; // Import your PDF file
 
 function About() {
+  const handleDownloadCV = async () => {
+    saveAs(localPdfFile, "Mahmoud Khairy CV.pdf");
+  };
+
   return (
     <>
       <div className="main-title" id="about">
@@ -28,15 +26,42 @@ function About() {
               <img src={require("../../Images/portfolio.png")} alt="" />
             </Roll>
 
-            <div className="content row col-sm-12 col-md-7">
+            <div
+              className="content row col-sm-12 col-md-7"
+              style={{ height: "300px" }}
+            >
               <div className="info col-5">
-                <p className="about-me">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Quaerat voluptatem voluptates perferendis vero est modi
-                  corrupti quae ratione, voluptate maxime veritatis ad optio
-                  odit ipsa dignissimos
+                <p
+                  className="about-me"
+                  style={{
+                    maxHeight: "200px",
+                    overflow: "scroll",
+                    textAlign: "left",
+                  }}
+                >
+                  As a dynamic and fresh frontend developer, I specialize in
+                  crafting engaging user interfaces using ReactJS. With over a
+                  year of hands-on experience, I am deeply passionate about
+                  creating dynamic user experiences and adept at leveraging
+                  media queries for responsive design. My keen eye for detail
+                  ensures pixel-perfect execution, while my commitment to
+                  continuous learning drives me to stay updated with the latest
+                  trends and techniques. I have practical experience working
+                  with Figma for design collaboration and Next.js for building
+                  powerful web applications. Additionally, my proficiency in
+                  server-side rendering (SSR) and SEO optimization further
+                  enhances my ability to deliver high-performance websites. With
+                  a strong foundation in ReactJS and a knack for
+                  problem-solving, I am poised to collaborate with you to bring
+                  your digital projects to life with innovation and efficiency.
+                  Let's join forces and create something extraordinary together!
                 </p>
-                <div className="download-cv btn btn-danger">Download Cv</div>
+                <div
+                  className="download-cv btn btn-danger"
+                  onClick={handleDownloadCV}
+                >
+                  Download Cv
+                </div>
               </div>
               <div className="bars col-7">
                 <div className="Frontend">
