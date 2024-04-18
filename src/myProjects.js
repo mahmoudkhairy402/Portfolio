a8b5820510cc2724524c34fd86ae59072c423fde
Mahmoud khairy <mahmoudkhairy402@gmail.com>
2024-04-18 22:52:17 +0200

    add latest project (e-commerce using redux)

--- src/myProjects.js
+++ src/myProjects.js
@@ -1,32 +1,30 @@
 export const myProjects = [
   {
-    projectName: "leon landing page",
-    projectImg: require("./Images/template1.png"),
-    projectDesc: "template 1 using Html & css",
-    projectLink:
-      "https://mahmoudkhairy402.github.io/Html-Css--Template--1/template1.html",
-    rebo: "https://github.com/mahmoudkhairy402/Html-Css--Template--1",
-    category: ["css"],
+    projectName: "E-Commerce",
+    projectImg: require("./Images/E-Commerce.png"),
+    projectDesc: "Complete E-Commerce Using Redux-Toolkit",
+    projectLink: "https://ecommerce-7861a.web.app/",
+    rebo: "https://github.com/mahmoudkhairy402/ecommerceredux",
+    category: ["bootstrap", "react", "sass", "redux"],
   },
   {
-    projectName: "kasper landing page",
-    projectImg: require("./Images/template2.png"),
-    projectDesc: "template 2 using Html & css",
-    projectLink:
-      "https://mahmoudkhairy402.github.io/Html-Css--Template--2/template2.html",
-    rebo: "https://github.com/mahmoudkhairy402/Html-Css--Template--2",
-    category: ["css"],
+    projectName: "E-Commerce ITI",
+    projectImg: require("./Images/e-commerce iti.png"),
+    projectDesc:
+      "Graduation project from Iti summer training (advanced frontend using reactjs) ",
+    projectLink: "https://github.com/mahmoudkhairy402/E-commerce-ITI",
+    rebo: "https://github.com/mahmoudkhairy402/E-commerce-ITI",
+    category: ["bootstrap", "react", "team"],
   },
   {
-    projectName: "landing page",
-    projectImg: require("./Images/template3.png"),
-    projectDesc: "template 3 using Html & css",
-    projectLink:
-      "https://mahmoudkhairy402.github.io/Html-Css--Template--3/template3.html",
-    rebo: "https://github.com/mahmoudkhairy402/Html-Css--Template--3",
-    category: ["css"],
+    projectName: "Prayer timer",
+    projectImg: require("./Images/prayer.png"),
+    projectDesc:
+      "prayer app for calculating pray time and the remaining time for every pray ,\n using react with vite , Mui",
+    projectLink: "https://vocal-macaron-068dd4.netlify.app",
+    rebo: "https://github.com/mahmoudkhairy402/prayer-timing",
+    category: ["mui", "react"],
   },
-
   {
     projectName: "Product Managment System",
     projectImg: require("./Images/cruds.png"),
@@ -85,30 +83,38 @@ export const myProjects = [
     category: ["bootstrap", "react"],
   },
   {
-    projectName: "E-Commerce ITI",
-    projectImg: require("./Images/e-commerce iti.png"),
-    projectDesc:
-      "Graduation project from Iti summer training (advanced frontend using reactjs) ",
-    projectLink: "https://github.com/mahmoudkhairy402/E-commerce-ITI",
-    rebo: "https://github.com/mahmoudkhairy402/E-commerce-ITI",
-    category: ["bootstrap", "react", "team"],
-  },
-
-  {
-    projectName: "Prayer timer",
-    projectImg: require("./Images/prayer.png"),
-    projectDesc:
-      "prayer app for calculating pray time and the remaining time for every pray ,\n using react with vite , Mui",
-    projectLink: "https://vocal-macaron-068dd4.netlify.app",
-    rebo: "https://github.com/mahmoudkhairy402/prayer-timing",
-    category: ["mui", "react"],
-  },
-  {
     projectName: "Awesome portfolio",
     projectImg: require("./Images/portfolioImg.png"),
     projectDesc: "Awesome portfolio using reactjs ",
     projectLink: "https://portfolio-react-3666b.web.app",
     rebo: "https://github.com/mahmoudkhairy402/Portfolio",
     category: ["react"],
+  },
+  {
+    projectName: "leon landing page",
+    projectImg: require("./Images/template1.png"),
+    projectDesc: "template 1 using Html & css",
+    projectLink:
+      "https://mahmoudkhairy402.github.io/Html-Css--Template--1/template1.html",
+    rebo: "https://github.com/mahmoudkhairy402/Html-Css--Template--1",
+    category: ["css"],
+  },
+  {
+    projectName: "kasper landing page",
+    projectImg: require("./Images/template2.png"),
+    projectDesc: "template 2 using Html & css",
+    projectLink:
+      "https://mahmoudkhairy402.github.io/Html-Css--Template--2/template2.html",
+    rebo: "https://github.com/mahmoudkhairy402/Html-Css--Template--2",
+    category: ["css"],
+  },
+  {
+    projectName: "landing page",
+    projectImg: require("./Images/template3.png"),
+    projectDesc: "template 3 using Html & css",
+    projectLink:
+      "https://mahmoudkhairy402.github.io/Html-Css--Template--3/template3.html",
+    rebo: "https://github.com/mahmoudkhairy402/Html-Css--Template--3",
+    category: ["css"],
   },
 ];
